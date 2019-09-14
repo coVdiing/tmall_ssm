c2cdcb620f083a8010e848c0fdcc511407a5ffcd
coVdiing <113714806@qq.com>
2019-09-14 12:27:02 +0800

    [edit][dao] 修改CategoryMapper，用逆向工程自动生成

--- src/main/java/com/vi/tmall/mapper/CategoryMapper.java
+++ src/main/java/com/vi/tmall/mapper/CategoryMapper.java
@@ -1,13 +1,21 @@
 package com.vi.tmall.mapper;
 
 import com.vi.tmall.pojo.Category;
-
+import com.vi.tmall.pojo.CategoryExample;
 import java.util.List;
 
 public interface CategoryMapper {
-    List<Category> list();
-    void add(Category category);
-    void delete(int id);
-    Category get(int id);
-    void update(Category category);
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    List<Category> selectByExample(CategoryExample example);
+
+    Category selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
 }

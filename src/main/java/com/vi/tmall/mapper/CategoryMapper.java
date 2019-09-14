79eb23f95759a0ea1784ef23fec4fb76835658de
coVdiing <113714806@qq.com>
2019-09-14 10:19:38 +0800

    [edit][dao] 去掉getTotal，修改list，去掉方法中的参数Page

--- src/main/java/com/vi/tmall/mapper/CategoryMapper.java
+++ src/main/java/com/vi/tmall/mapper/CategoryMapper.java
@@ -1,14 +1,11 @@
 package com.vi.tmall.mapper;
 
 import com.vi.tmall.pojo.Category;
-import com.vi.tmall.util.Page;
-
 
 import java.util.List;
 
 public interface CategoryMapper {
-    List<Category> list(Page page);
-    int getTotal();
+    List<Category> list();
     void add(Category category);
     void delete(int id);
     Category get(int id);

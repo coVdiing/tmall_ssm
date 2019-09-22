fc30f90310b97269901d27cd3beec21df1e87678
coVdiing <113714806@qq.com>
2019-09-22 12:25:53 +0800

    [edit][Service] CategoryMapper,修改了更新使用的方法，必须使用updateByPrimaryKeySelective,否则传入的对象如果部分数据为空，导致原数据被删除！！！

--- src/main/java/com/vi/tmall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/vi/tmall/service/impl/OrderServiceImpl.java
@@ -30,7 +30,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public void update(Order order) {
-        orderMapper.updateByPrimaryKey(order);
+        orderMapper.updateByPrimaryKeySelective(order);
     }
 
     @Override
